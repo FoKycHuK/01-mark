735a5ddde483e776b1f6895c137b140f231a6e36
FoKycHuK <fokychuk47@ya.ru>
2014-11-02 05:40:36 +0600

    Скелет. Метод Main. Библиотека NUnit

---
+++ 01-mark/ParserToHTML.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_mark
+{
+    class ParserToHTML
+    {
+        public static string[] Parse(string text)
+        {
+            var replacedSpecial = ParseSpecialSymbols(text);
+            var lines = ParseLines(replacedSpecial);
+            lines = ParseBackticks(lines);
+            lines = ParseUnderlines(lines);
+            lines = ParseDoubleUnderlines(lines);
+            lines = RemoveEscapeChars(lines);
+            return lines;
+        }
+        public static string ParseSpecialSymbols(string text)
+        {
+            throw new NotImplementedException();
+        }
+        public static string[] ParseLines(string text)
+        {
+            throw new NotImplementedException();
+        }
+        public static string[] ParseBackticks(string[] lines) //временный коммент. хочу тут добавлять к каждому спец символу экранизацию (/)
+        {
+            throw new NotImplementedException();
+        }
+        public static string[] ParseUnderlines(string[] lines) //временный коммент. хочу тут реализовывать неэкранированные подчеркивания.
+        {
+            throw new NotImplementedException();
+        }
+        public static string[] ParseDoubleUnderlines(string[] lines) //временный коммент. хочу тут реализовывать неэкранированные двойные подчеркивания.
+        {
+            throw new NotImplementedException();
+        }
+        public static string[] RemoveEscapeChars(string[] lines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 01-mark/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace _01_mark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFileName =  "../../../";
+            var outputFileName = "output.html";
+            if (args.Length == 0)
+            {
+                Console.Write("Enter input file name: ");
+                inputFileName += Console.ReadLine();
+            }
+            else
+                inputFileName += args[0];
+            if (args.Length >= 2)
+                outputFileName = args[1];
+
+        }
+        static string ReadDataFromFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+        static void WriteDataFromFile(string[] data, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
