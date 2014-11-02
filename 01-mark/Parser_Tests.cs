2dba0c253ad43a7170f5a0798b3f232e3502b6f6
FoKycHuK <fokychuk47@ya.ru>
2014-11-03 04:42:18 +0600

    большой, трудоемкий рефакторинг тестов. должно стать понятнее.

--- 01-mark/Parser_Tests.cs
+++ 01-mark/Parser_Tests.cs
@@ -13,15 +13,13 @@ namespace _01_mark
         [Test]
         public void Simple_replace_special()
         {
-            string text;
-            text = "\"><";
+            var text = "\"><";
             Assert.AreEqual("&quot;&gt;&lt;", ParserToHTML.ParseSpecialSymbols(text));
         }
         [Test]
         public void Complex_replace_special()
         {
-            string text;
-            text = "<p> super test >>> \"<\"";
+            var text = "<p> super test >>> \"<\"";
             Assert.AreEqual("&lt;p&gt; super test &gt;&gt;&gt; &quot;&lt;&quot;", ParserToHTML.ParseSpecialSymbols(text));
         }
         #endregion
@@ -31,186 +29,148 @@ namespace _01_mark
         [Test]
         public void Simple_parsing_lines()
         {
-            string text;
-            text = "Just one line";
+            var text = "Just one line";
             Assert.AreEqual(new string[] { "<p>Just one line</p>" }, ParserToHTML.ParseLines(text));
         }
         [Test]
         public void Complex_parsing_lines()
         {
-            string text;
-            text = "Just one line\nAnd another one.    \n And some spaces \n ";
+            var text = "Just one line\nAnd another one.    \n And some spaces \n ";
             Assert.AreEqual(new string[] { "<p>Just one line", "And another one.    ", " And some spaces ", " </p>" }, ParserToHTML.ParseLines(text));
         }
         [Test]
         public void Paragraph_divisions()
         {
-            string text;
-            text = "some here\n\nText\n   \n        \n \n";
+            var text = "some here\n\nText\n   \n        \n \n";
             Assert.AreEqual(new string[] { "<p>some here</p>", "<p>", "Text</p>", "   <p></p>", "        <p></p>", " <p>", "</p>" }, ParserToHTML.ParseLines(text));
         }
         #endregion
 
 
         #region Correct parsing backticks
+        void ParsingBackticksTest(string[] text, string[] ans)
+        {
+            ParserToHTML.ParseBackticks(text);
+            Assert.AreEqual(text, ans);
+        }
         [Test]
         public void Correct_insert_codes()
         {
-            var text = new string[1];
-            text[0] = "uncoded, `coded`";
-            ParserToHTML.ParseBackticks(text);
-            Assert.AreEqual(new string[] {"uncoded, <code>coded</code>"}, text);
+            ParsingBackticksTest(new string[] { "uncoded, `coded`" }, new string[] { "uncoded, <code>coded</code>" });
         }
         [Test]
         public void Correct_handle_one_backtick()
         {
-            var text = new string[1];
-            text[0] = "uncoded, `uncoded too";
-            ParserToHTML.ParseBackticks(text);
-            Assert.AreEqual(new string[] { "uncoded, `uncoded too" }, text);
+            ParsingBackticksTest(new string[] { "uncoded, `uncoded too" }, new string[] { "uncoded, `uncoded too" });
         }
         [Test]
         public void Correct_handle_many_lines()
         {
-            var text = new string[2];
-            text[0] = "uncoded, `uncoded too";
-            text[1] = "and this uncoded too`";
-            ParserToHTML.ParseBackticks(text);
-            Assert.AreEqual(new string[] { "uncoded, `uncoded too", "and this uncoded too`" }, text);
+            ParsingBackticksTest(new string[] { "uncoded, `uncoded too", "and this uncoded too`" }, new string[] { "uncoded, `uncoded too", "and this uncoded too`" });
         }
         [Test]
         public void Correct_handle_many_lines_and_some_backticks()
         {
-            var text = new string[2];
-            text[0] = "`uncoded";
-            text[1] = "this `coded`";
-            ParserToHTML.ParseBackticks(text);
-            Assert.AreEqual(new string[] { "`uncoded", "this <code>coded</code>" }, text);
+            ParsingBackticksTest(new string[] { "`uncoded", "this `coded`" }, new string[] { "`uncoded", "this <code>coded</code>" });
         }
         [Test]
         public void Correct_handle_underlines()
         {
-            var text = new string[1];
-            text[0] = "_uncoded_, `_coded_`";
-            ParserToHTML.ParseBackticks(text);
-            Assert.AreEqual(new string[] { @"_uncoded_, <code>\_coded\_</code>" }, text);
+            ParsingBackticksTest(new string[] { "_uncoded_, `_coded_`" }, new string[] { "_uncoded_, <code>\\_coded\\_</code>" });
         }
         [Test]
         public void Correct_handle_many_underlines()
         {
-            var text = new string[1];
-            text[0] = "`_this_is_it____`";
-            ParserToHTML.ParseBackticks(text);
-            Assert.AreEqual(new string[] { @"<code>\_this\_is\_it\_\_\_\_</code>" }, text);
+            ParsingBackticksTest(new string[] { "`_this_is_it____`" }, new string[] { "<code>\\_this\\_is\\_it\\_\\_\\_\\_</code>" });
         }
         #endregion
 
 
         #region Correct parsing double underlines
+        void ParsingDoubleUnderlineTest(string[] text, string[] ans)
+        {
+            ParserToHTML.ParseDoubleUnderlines(text);
+            Assert.AreEqual(text, ans);
+        }
         [Test]
         public void Simple_double_underlines()
         {
-            var text = new string[1];
-            text[0] = "__THIS IS STRONG__";
-            ParserToHTML.ParseDoubleUnderlines(text);
-            Assert.AreEqual(new string[] { "<strong>THIS IS STRONG</strong>" }, text);
+            ParsingDoubleUnderlineTest(new string[] { "__THIS IS STRONG__" }, new string[] { "<strong>THIS IS STRONG</strong>" });
         }
         [Test]
         public void Complex_double_underlines()
         {
-            var text = new string[1];
-            text[0] = "_norm__ __THIS__IS______STRONG__ __small(cause 3 underline, not 2)___";
-            ParserToHTML.ParseDoubleUnderlines(text);
-            Assert.AreEqual(new string[] { "_norm__ <strong>THIS__IS______STRONG</strong> __small(cause 3 underline, not 2)___" }, text);
+            ParsingDoubleUnderlineTest(new string[] { "_norm__ __THIS__IS______STRONG__ __small(cause 3 underline, not 2)___" }, 
+                new string[] { "_norm__ <strong>THIS__IS______STRONG</strong> __small(cause 3 underline, not 2)___" });
         }
         [Test]
         public void Avoid_tagged()
         {
-            var text = new string[1];
-            text[0] = "\\__norm__ __norm\\__";
-            ParserToHTML.ParseDoubleUnderlines(text);
-            Assert.AreEqual(new string[] { "\\__norm__ __norm\\__" }, text);
+            ParsingDoubleUnderlineTest(new string[] { "\\__norm__ __norm\\__" }, new string[] { "\\__norm__ __norm\\__" });
         }
         #endregion
 
 
         #region Correct parsing underlines
+        void ParsingUnderlineTest(string[] text, string[] ans)
+        {
+            ParserToHTML.ParseUnderlines(text);
+            Assert.AreEqual(text, ans);
+        }
         [Test]
         public void Simple_underlines()
         {
-            var text = new string[1];
-            text[0] = "some, _SOME_ , some";
-            ParserToHTML.ParseUnderlines(text);
-            Assert.AreEqual(new string[] { "some, <em>SOME</em> , some" }, text);
+            ParsingUnderlineTest(new string[] { "some, _SOME_ , some" }, new string[] { "some, <em>SOME</em> , some" });
         }
         [Test]
         public void Complex_underlines()
         {
-            var text = new string[1];
-            text[0] = "bla, _bla_bla_ bla _aa_____qqq_";
-            ParserToHTML.ParseUnderlines(text);
-            Assert.AreEqual(new string[] { "bla, <em>bla_bla</em> bla <em>aa_____qqq</em>" }, text);
+            ParsingUnderlineTest(new string[] { "bla, _bla_bla_ bla _aa_____qqq_" }, 
+                new string[] { "bla, <em>bla_bla</em> bla <em>aa_____qqq</em>" });
         }
         [Test]
         public void Avoid_tagged_underlines()
         {
-            var text = new string[1];
-            text[0] = "bla, \\_bla_bla_ bla";
-            ParserToHTML.ParseUnderlines(text);
-            Assert.AreEqual(new string[] { "bla, \\_bla_bla_ bla" }, text);
+            ParsingUnderlineTest(new string[] { "bla, \\_bla_bla_ bla" }, new string[] { "bla, \\_bla_bla_ bla" });
         }
         [Test]
         public void Must_to_parse_underline_with_dots()
         {
-            var text = new string[1];
-            text[0] = "_simple_.";
-            ParserToHTML.ParseUnderlines(text);
-            Assert.AreEqual(new string[] { "<em>simple</em>." }, text);
+            ParsingUnderlineTest(new string[] { "_simple_." }, new string[] { "<em>simple</em>." });
         }
         #endregion
 
 
         #region Correct removing escape symbol (\)
+        void RemovingEscapeTest(string[] text, string[] ans)
+        {
+            ParserToHTML.RemoveEscapeChars(text);
+            Assert.AreEqual(text, ans);
+        }
         [Test]
         public void Remove_single_symbols()
         {
-            var text = new string[1];
-            text[0] = "\\ something, \\_removing it all";
-            ParserToHTML.RemoveEscapeChars(text);
-            Assert.AreEqual(new string[] { " something, _removing it all" }, text);
+            RemovingEscapeTest(new string[] { "\\ something, \\_removing it all" }, new string[] { " something, _removing it all" });
         }
         [Test]
         public void Remove_single_symbols_in_dif_lines()
         {
-            var text = new string[2];
-            text[0] = "begin\\";
-            text[1] = "\\end";
-            ParserToHTML.RemoveEscapeChars(text);
-            Assert.AreEqual(new string[] { "begin", "end" }, text);
+            RemovingEscapeTest(new string[] { "begin\\", "\\end" }, new string[] { "begin", "end" });
         }
         [Test]
         public void Pass_double_symbols()
         {
-            var text = new string[1];
-            text[0] = "some\\\\thing";
-            ParserToHTML.RemoveEscapeChars(text);
-            Assert.AreEqual(new string[] { "some\\thing" }, text);
+            RemovingEscapeTest(new string[] { "some\\\\thing" }, new string[] { "some\\thing" });
         }
         [Test]
         public void Pass_double_symbols_double()
         {
-            var text = new string[1];
-            text[0] = "some\\\\\\\\thing";
-            ParserToHTML.RemoveEscapeChars(text);
-            Assert.AreEqual(new string[] { "some\\\\thing" }, text);
+            RemovingEscapeTest(new string[] { "some\\\\\\\\thing" }, new string[] { "some\\\\thing" });
         }
         [Test]
         public void Remove_single_after_double()
         {
-            var text = new string[1];
-            text[0] = "some\\\\\\thing";
-            ParserToHTML.RemoveEscapeChars(text);
-            Assert.AreEqual(new string[] { "some\\thing" }, text);
+            RemovingEscapeTest(new string[] { "some\\\\\\thing" }, new string[] { "some\\thing" });
         }
         #endregion
     }
